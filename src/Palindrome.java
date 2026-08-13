4a0a60af25ccddd1437b7aab19ec2fb1b9565c0b
CloudCodingSpace <subhamaygamer2127@outlook.com>
2026-08-13 21:42:40 +0530

    A small change

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -4,6 +4,7 @@ public class Palindrome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        /* Using stringbuilder
         System.out.println("Enter a number :- ");
         String a = String.valueOf(sc.nextInt());
 
@@ -14,6 +15,32 @@ public class Palindrome {
             System.out.println("The number is a palindrome number!");
         } else {
             System.out.println("The number isn't a palindrome number!");
+        }
+        */
+
+        // By my own method which I derived (not claiming to be original though)
+        System.out.println("Enter a number :- ");
+        String a = sc.nextLine().trim();
+
+        if(a.isEmpty()) {
+            System.out.println("You must enter a number!");
+            System.exit(-1);
+        }
+
+        boolean b = true;
+        int j = a.length() - 1;
+        for(int i = 0; i < a.length()/2; i++) {
+            if(a.charAt(i) != a.charAt(j)) {
+                b = false;
+                break;
+            }
+            j--;
+        }
+
+        if(b) {
+            System.out.println("Input is a palinedrome!");
+        } else {
+            System.out.println("Input isn't a palinedrome!");
         }
 
         sc.close();
--- src/ShortenName.java
+++ src/ShortenName.java
@@ -4,37 +4,44 @@ public class ShortenName {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your name :- ");
-        String name = sc.nextLine();
-        String lname = name.toLowerCase();
+        System.out.println("Enter your name (full name) :-");
+        String name = sc.nextLine().trim();
 
-        if(name.trim().isEmpty()) {
-            System.err.println("YOu must have a name man!!");
-            sc.close();
-            return;
+        if(name.isEmpty()) {
+            System.out.println("You must enter a name!");
+            System.exit(-1);
         }
 
-        String str = "";
-        int m = name.lastIndexOf(" ");
-        String sname = name.substring(m);
-        if(m != (name.length() - 1))
-            sname = name.substring(m+1);
+        int idx = name.lastIndexOf(" ");
+        
+        if(idx == -1) {
+             System.out.println("You are supposed to enter the full name!");
+             System.exit(-1);
+        }
 
-        boolean b = false;
-        for(int i = 0; i < m; i++) {
-            if((lname.charAt(i) <= 'z') && (lname.charAt(i) >= 'a')) {
-                if(b) {
+        String sname = name.substring(idx).trim();
+
+        if(sname.isEmpty() || name.substring(0, idx).isEmpty()) {
+            System.out.println("You are supposed to enter the full name!");
+            System.exit(-1);
+        }
+
+        String fname = name.substring(0, idx).toLowerCase();
+        String ss = "";
+        boolean a = false;
+        for(int i = 0; i < fname.length(); i++) {
+            if((fname.charAt(i) <= 'z') && (fname.charAt(i) >= 'a')) {
+                if(a) {
                     continue;
                 }
-                str += (char)('A' + (lname.charAt(i) - 'a')) + ". ";
-                b = true;
+                a = true;
+                ss += (char)('A' + (fname.charAt(i) - 'a')) + ". ";
             } else {
-                b = false;
+                a = false;
             }
         }
 
-        System.out.println(str + sname);
-
+        System.out.println("Shortened name :- " + ss + sname);
         sc.close();
     }
 }
